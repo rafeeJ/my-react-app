86b69887a249a7f4cf18d643ff855d7a7ba2f389
Rafee Jenkins <rafeejenkins@gmail.com>
2023-01-21 18:05:43 +0000

    feat: updates tests

--- src/functions/sum/sum.spec.js
+++ src/functions/sum/sum.spec.js
@@ -1,7 +1,7 @@
 const sum = require('./sum')
 
 test('should add two numbers together', () => { 
-    expect(sum(1, 2)).toBe(4) 
+    expect(sum(1, 2)).toBe(5) 
 })
 
 test('result should be undefined witht two strings', () => { 
